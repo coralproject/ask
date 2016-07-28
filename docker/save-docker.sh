dcf9603a2fe9825bcffda87b427e90608d092eae
gaba <gabelula@gmail.com>
2016-07-28 11:44:46 -0700

    Elkhorn release

--- docker/save-docker.sh
+++ docker/save-docker.sh
@@ -1,4 +1,4 @@
-docker save coralproject/mongodata > mongo.tar
+docker save coralproject/mongodata > mongodata.tar
 docker save coralproject/pillar > pillar.tar
 docker save coralproject/elkhorn > elkhorn.tar
 docker save coralproject/cay > cay.tar
