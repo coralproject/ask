0e3b897aac79f0b5335aa0c71b9d3fd8f9ae28ca
Eugene de Fikh <defikh@gmail.com>
2016-08-10 16:59:04 +0200

    adding import and export script

---
+++ scripts/ask-export.sh
@@ -0,0 +1,3 @@
+mongodump --collection form_galleries --db coral --authenticationDatabase admin -u coraluser -p welcome
+mongodump --collection forms --db coral --authenticationDatabase admin -u coraluser -p welcome
+imongodump --collection form_submissions --db coral --authenticationDatabase admin -u coraluser -p welcome
---
+++ scripts/ask-import.sh
@@ -0,0 +1,1 @@
+mongorestore -u coral-user -p kiwort52 --authenticationDatabase admin -d coral  coral
