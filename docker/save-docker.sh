f374bd5b18f08827120c11abd28952aadb5e7c02
Jeff Nelson <jeff@cinchapi.com>
2016-10-18 13:30:48 -0400

    remove install scripts that don't work

--- docker/save-docker.sh
+++
@@ -1,4 +0,0 @@
-docker save coralproject/mongodata > mongodata.tar
-docker save coralproject/pillar > pillar.tar
-docker save coralproject/elkhorn > elkhorn.tar
-docker save coralproject/cay > cay.tar
--- scripts/ask-import.sh
+++
@@ -1,1 +0,0 @@
-mongorestore -u coral-user -p kiwort52 --authenticationDatabase admin -d coral  coral
